fb60adea27dacfe7334ce7409f4c50be4c824e46
Chidozie Ijeomah <bendozy@gmail.com>
2018-06-30 20:16:43 +0100

    write tests for RequestPaths

--- jest.config.js
+++ jest.config.js
@@ -1,12 +1,5 @@
 module.exports = {
   displayName: 'Swagger Proccessor Tests',
-  // collectCoverageFrom: [!'<rootDir>/app/tests/*.js'],
-  collectCoverageFrom: [
-    '<rootDir>/app/**/*.js',
-    '!<rootDir>/app/tests/*.js',
-    '!<rootDir>/app/store/*.js',
-    '!<rootDir>/app/utils/*.js',
-  ],
   moduleNameMapper: {
     '\\.css$': 'identity-obj-proxy',
   },
